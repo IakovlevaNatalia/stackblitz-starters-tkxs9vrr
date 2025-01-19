afc0c72cb828859184b427cad698ac3d1a22f4fc
IakovlevaNatalia <tourmalina@inbox.ru>
2025-01-19 10:51:30 -0500

    Added subscription feature

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log('Hello!');
+/*JavaScript to show alert message*/
+document.getElementById('subscribe-form').addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent form submission
+  alert('Thank you for subscribing.');
+});
